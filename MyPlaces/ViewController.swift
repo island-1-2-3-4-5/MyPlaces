e33a8a38db8621a76b1ffdcb02354079c96ae162
Roman <r.monahov2014@yandex.ru>
2020-06-03 12:53:27 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Roman on 03.06.2020.
+//  Copyright © 2020 Roman Monakhov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
